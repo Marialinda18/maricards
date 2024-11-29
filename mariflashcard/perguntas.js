845d2e0984af63ae29098906b3d6d81da756a5c5
Marialinda18 <00001136575091sp@al.educacao.sp.gov.br>
2024-11-29 13:53:07 -0300

    Update perguntas.js

--- mariflashcard/perguntas.js
+++ mariflashcard/perguntas.js
@@ -1,23 +1,24 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Academia',
+    'O que é a creatina?',
+    'é um composto de aminoácidos. Tem sido considerada um recurso ergogênico eficaz devido auxílio no ganho de massa muscular e melhora do desempenho físico.'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Maquiagem',
+    'O que é uma base de maquigaem?',
+    '
+A base é um produto cosmético utilizado para igualar o tom da pele, assim como para amenizar manchas e imperfeições do rosto'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Como se diz como você está? em Inglês?',
+    'como você está? em ingles é How are you?'
 )
